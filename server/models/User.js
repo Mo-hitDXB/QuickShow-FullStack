82e28ef43321f9717d172c1a404321551e6d0334
Mohit <mohit.harpalani@gisma-student.com>
2025-09-25 05:36:13 +0200

    updated file

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,12 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  image: { type: String, required: true },
-  password: { type: String, required: true }   // ✅ add password field
-});
+    _id: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    image: {type: String, required: true}
+})
 
-// 🔒 Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next(); // only hash if changed
-  this.password = await bcrypt.hash(this.password, 12); // cost factor = 12
-  next();
-});
-
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
 export default User;
